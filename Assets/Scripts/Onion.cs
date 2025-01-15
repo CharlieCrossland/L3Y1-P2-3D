ebdac3947601c1d5021c2b88da6cc0e4659136c2
CharlieCrossland <cro24003689@heartofyorkshire.ac.uk>
2025-01-15 16:10:22 +0000

    Tried making onion enemy, Failed

---
+++ Assets/Scripts/Onion.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Onion : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool spawnEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("endPath"))
+        {
+            Destroy(this.gameObject);
+            spawnEnemy = true;
+        }
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
