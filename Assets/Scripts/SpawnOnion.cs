1b81316b68d38eb710410b566bb66a4c8419d4bd
CharlieCrossland <cro24003689@heartofyorkshire.ac.uk>
2025-01-16 13:59:51 +0000

    Took Screenshot of models, removed broken code from SpawnOnion Script

--- Assets/Scripts/SpawnOnion.cs
+++ Assets/Scripts/SpawnOnion.cs
@@ -19,7 +19,7 @@ public class SpawnOnion : MonoBehaviour
     {
         if (OnionScript.spawnEnemy == true)
         {
-            Instantiate(OnionModel, SpawnPoint);
+           
         }
     }
 }
